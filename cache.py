55565faae62ef7ba98f9887d9b2d78c8e6f6acaf
Riva <rivanathans@gmail.com>
2018-02-17 11:07:16 +0100

    read initial constants

--- cache.py
+++ cache.py
@@ -4,5 +4,12 @@ import numpy as np
 import sys
 
 with open('../data/kittens.in', 'r') as file:
-	for line in file:
-		print(line)
+	line = file.readline().strip().split(' ')
+
+	numVideos = int(line[0])
+	numEndpoints = int(line[1])
+	numRequests = int(line[2])
+	numCaches = int(line[3])
+	cacheSize = int(line[4])
+
+	print(numVideos, numEndpoints, numRequests, numCaches, cacheSize)
