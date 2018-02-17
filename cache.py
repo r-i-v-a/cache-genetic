2d9251c97520a4ad7b56f49ed8969b19f3ae465b
Riva <rivanathans@gmail.com>
2018-02-17 15:10:01 +0100

    store connected caches + time saved for each endpoint

--- cache.py
+++ cache.py
@@ -3,14 +3,17 @@
 import numpy as np
 import sys
 
-with open('../data/kittens.in', 'r') as file:
-	line = file.readline().strip().split(' ')
+def readLineAsNumbers(file):
+	return list(map(int, file.readline().strip().split(' ')))
 
-	numVideos = int(line[0])
-	numEndpoints = int(line[1])
-	numRequests = int(line[2])
-	numCaches = int(line[3])
-	cacheSize = int(line[4])
+with open('../data/me_at_the_zoo.in', 'r') as file:
+	line = readLineAsNumbers(file)
+
+	numVideos = line[0]
+	numEndpoints = line[1]
+	numRequests = line[2]
+	numCaches = line[3]
+	cacheSize = line[4]
 
 	print('\n# videos:', numVideos)
 	print('# endpoints:', numEndpoints)
@@ -18,7 +21,24 @@ with open('../data/kittens.in', 'r') as file:
 	print('# caches:', numCaches)
 	print('# cache size:', cacheSize)
 
-	line = file.readline().strip().split(' ')
-	videoSizes = np.array(list(map(int, line)))
+	videoSizes = np.array(readLineAsNumbers(file))
 
 	print('\nvideo sizes:', videoSizes)
+
+	cacheLatency = []
+
+	for i in range(numEndpoints):
+		line = readLineAsNumbers(file)
+		dataCenterLatency = line[0]
+		numConnectedCaches = line[1]
+		endpointCaches = []
+
+		for j in range(numConnectedCaches):
+			line = readLineAsNumbers(file)
+			endpointCaches.append((line[0], dataCenterLatency - line[1]))
+
+		endpointCaches = sorted(endpointCaches, key=lambda x: x[1], reverse=True)
+		cacheLatency.append(endpointCaches)
+
+		print('\nendpoint:', i)
+		print('connected caches:', endpointCaches)
