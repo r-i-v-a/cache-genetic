8594a61a018ba2e8c020fac4b6d0c8fccd7f7044
Riva <rivanathans@gmail.com>
2018-02-17 15:35:19 +0100

    read in data

--- cache.py
+++ cache.py
@@ -6,25 +6,10 @@ import sys
 def readLineAsNumbers(file):
 	return list(map(int, file.readline().strip().split(' ')))
 
-with open('../data/me_at_the_zoo.in', 'r') as file:
-	line = readLineAsNumbers(file)
+def readVideoSizes(file):
+	return np.array(readLineAsNumbers(file))
 
-	numVideos = line[0]
-	numEndpoints = line[1]
-	numRequests = line[2]
-	numCaches = line[3]
-	cacheSize = line[4]
-
-	print('\n# videos:', numVideos)
-	print('# endpoints:', numEndpoints)
-	print('# request descriptions:', numRequests)
-	print('# caches:', numCaches)
-	print('# cache size:', cacheSize)
-
-	videoSizes = np.array(readLineAsNumbers(file))
-
-	print('\nvideo sizes:', videoSizes)
-
+def readCacheLatency(file, numEndpoints):
 	cacheLatency = []
 
 	for i in range(numEndpoints):
@@ -40,5 +25,38 @@ with open('../data/me_at_the_zoo.in', 'r') as file:
 		endpointCaches = sorted(endpointCaches, key=lambda x: x[1], reverse=True)
 		cacheLatency.append(endpointCaches)
 
-		print('\nendpoint:', i)
-		print('connected caches:', endpointCaches)
+	return cacheLatency
+
+def readRequests(file, numRequests):
+	requests = []
+
+	for i in range(numRequests):
+		line = readLineAsNumbers(file)
+		requests.append((line[0], line[1], line[2]))
+
+	return requests
+
+with open('../data/me_at_the_zoo.in', 'r') as file:
+	line = readLineAsNumbers(file)
+
+	numVideos = line[0]
+	numEndpoints = line[1]
+	numRequests = line[2]
+	numCaches = line[3]
+	cacheSize = line[4]
+
+	print('\n# videos:', numVideos)
+	print('# endpoints:', numEndpoints)
+	print('# request descriptions:', numRequests)
+	print('# caches:', numCaches)
+	print('# cache size:', cacheSize)
+
+	videoSizes = readVideoSizes(file)
+	print('\nvideo sizes:', videoSizes)
+
+	cacheLatency = readCacheLatency(file, numEndpoints)
+	print('\ncache latencies:', cacheLatency)
+
+	requests = readRequests(file, numRequests)
+	print('\nrequests:', requests)
+	
